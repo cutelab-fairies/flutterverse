b33b6aede3c3d113e0be114845fa17ab80e5913b
Maki <mxmcube@gmail.com>
2019-03-28 20:44:00 -0700

    Commiting from another machine

--- src/main/java/Flutterverse.java
+++ src/main/java/Flutterverse.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 public class Flutterverse {
     public static void main(String[] args) throws IOException {
+        System.out.println("Starting the Flutterverse engine!");
         Engine engine = new Engine();
         engine.run(engine);
     }
